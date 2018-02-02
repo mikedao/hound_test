021aa47fc025b7a423ac99d1fa2b7d0c9dbd8409
Michael Dao <mike@turing.io>
2018-02-02 11:46:49 -0700

    Forgive me

---
+++ code.rb
@@ -0,0 +1,29 @@
+
+
+
+
+
+class Thing
+
+              def initialize 
+                @something = nil
+              end
+
+        def method
+
+          
+          hello = 0
+        
+        
+        
+        
+        
+        
+        
+        
+        end
+
+      
+  end
+
+  puts "hello mom"
